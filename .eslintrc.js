51bebad56ad222666b3a0309ed8c2cb65106cdf4
Rakshitaaa <rakshitakanwar09@gmail.com>
2024-03-15 11:16:15 +0530

    backend removed

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  parserOptions: {
-    ecmaVersion: 2021, // Adjust the version as needed
-  },
-  rules: {
-    // Add your ESLint rules here
-  },
-};
--- middlewares/uploadImage.js
+++
@@ -1,7 +0,0 @@
-const multer = require('multer');
-
-// Set storage engine
-const storage = multer.memoryStorage(); // To store files in memory (buffer)
-
-// Initialize Multer
-exports.upload = multer({ storage: storage });
