c95b46a5d168413023ee1d6c2d29ff4eea88776d
Rakshitaaa <rakshitakanwar09@gmail.com>
2024-03-28 11:13:49 +0530

    configs

--- src/config.js
+++ src/config.js
@@ -1,14 +1,30 @@
-const NODE_ENV = "dev";
+let configPromise;
 
-module.exports = {
-  frontend_url:
-    NODE_ENV === "production"
-      ? "https://counsellor.sortmycollege.com"
-      : "https://localhost:3000",
-  // : "https://192.168.0.36:3000",
-  backend_url:
-    NODE_ENV === "production"
-      ? "https://server.sortmycollege.com"
-      : "http://localhost:9000",
-  // : "http://192.168.0.36:9000",
-};
+if (ProcessingInstruction.env.Node_ENV === 'production') {
+  configPromise = import('./config.prod.json').then(module => module.default);
+} else {
+  configPromise = import('./config.dev.json').then(module => module.default);
+}
+
+const config = await configPromise;
+
+export default config;
+
+
+
+
+
+// const NODE_ENV = "dev";
+
+// module.exports = {
+//   frontend_url:
+//     NODE_ENV === "production"
+//       ? "https://counsellor.sortmycollege.com"
+//       : "https://localhost:3000",
+//   // : "https://192.168.0.36:3000",
+//   backend_url:
+//     NODE_ENV === "production"
+//       ? "https://server.sortmycollege.com"
+//       : "http://localhost:9000",
+//   // : "http://192.168.0.36:9000",
+// };
