84fc4e9709e572363ec9cccb28f8974b49fa7c00
Rakshitaaa <rakshitakanwar09@gmail.com>
2024-04-02 17:55:37 +0530

    key feature addition apis and all

--- src/pages/keyFeatures/index.jsx
+++ src/pages/keyFeatures/index.jsx
@@ -1,40 +1,54 @@
-import './style.scss'
-import KeyFeaturesChildren from '../../components/keyFeaturesChildren'
-import png_1 from '../../assets/nature_6007468.png'
-import png_2 from '../../assets/loan_12292787.png'
-import png_3 from '../../assets/3.png'
-import png_4 from '../../assets/4.png'
-import png_5 from '../../assets/5.png'
+import './style.scss';
+import KeyFeaturesChildren from '../../components/keyFeaturesChildren';
+import axios from "axios";
+import config from "@/config"
+import { useState, useEffect, useContext } from "react";
+import { UserContext } from '../../context/UserContext';
+import { ProfileContext } from '../../context/ProfileContext';
+const { backend_url } = config;
 
 const KeyFeatures = () => {
+    const [keyFeaturesInstitute, setKeyFeaturesInstitute] = useState([]);
+    const {user} = useContext (UserContext);
+    const {editKeyFeatureEnable, setEditKeyFeatureEnable} = useContext(ProfileContext)
+    const getKeyFeatures = async () => {
+        try{
+            const { data } = await axios.get(`${backend_url}/ep/key-features`, {
+                headers:{
+                    Authorization : user.token
+                }
+            });
+            console.log(data);
+            setKeyFeaturesInstitute(data);
+        } catch (error) {
+            console.log("Error Fetching Key Features")
+        };
+    }
+    useEffect(() => {
+        getKeyFeatures();
+    }, []);
+
+
     return(
         <div className="key-features-parent">
             <div className="key-features-head">
                 <h1>Key Features</h1>
-                <button>Add New</button>
+                <button onClick={() => setEditKeyFeatureEnable(true)}>Add New</button>
             </div>
-            <div className="key-features-child">
-                <KeyFeaturesChildren featureName='Inclusive Environment' featurePng={ png_1 } />
-                <KeyFeaturesChildren featureName='Affordability and Financial Aid' featurePng={ png_2 } />
-                <KeyFeaturesChildren featureName='Extracurricular Activities' featurePng={ png_3 } />
-                <KeyFeaturesChildren featureName='Community Engagement' featurePng={ png_4 } />
-                <KeyFeaturesChildren featureName='Research Opportunities' featurePng={ png_5 } />
-                <KeyFeaturesChildren featureName='Extracurricular Activities' featurePng={ png_3 } />
-                <KeyFeaturesChildren featureName='Research Opportunities' featurePng={ png_5 } />
-                <KeyFeaturesChildren featureName='Inclusive Environment' featurePng={ png_1 } />
-                <KeyFeaturesChildren featureName='Affordability and Financial Aid' featurePng={ png_2 } />
-                <KeyFeaturesChildren featureName='Community Engagement' featurePng={ png_4 } />
-                <KeyFeaturesChildren featureName='Inclusive Environment' featurePng={ png_1 } />
-                <KeyFeaturesChildren featureName='Affordability and Financial Aid' featurePng={ png_2 } />
-                <KeyFeaturesChildren featureName='Extracurricular Activities' featurePng={ png_3 } />
-                <KeyFeaturesChildren featureName='Community Engagement' featurePng={ png_4 } />
-                <KeyFeaturesChildren featureName='Research Opportunities' featurePng={ png_5 } />
-                <KeyFeaturesChildren featureName='Extracurricular Activities' featurePng={ png_3 } />
-                <KeyFeaturesChildren featureName='Research Opportunities' featurePng={ png_5 } />
-                <KeyFeaturesChildren featureName='Inclusive Environment' featurePng={ png_1 } />
-                <KeyFeaturesChildren featureName='Affordability and Financial Aid' featurePng={ png_2 } />
-                <KeyFeaturesChildren featureName='Community Engagement' featurePng={ png_4 } />
-            </div>
+            {editKeyFeatureEnable ? 
+                <>
+
+                </> 
+            :
+                <div className="key-features-child">
+                    {keyFeaturesInstitute.map((keyFeatureInstitute, i) => {
+                        return (
+                            <KeyFeaturesChildren featureName={keyFeatureInstitute.name} featurePng={keyFeatureInstitute.key_features_icon} />
+                        )
+                    })}
+                </div>
+            }
+
         </div>
     )
 }
