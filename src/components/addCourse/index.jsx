7be561d47512e42c7c88297e09730638220145a7
khanNameera17 <92721716+khanNameera17@users.noreply.github.com>
2024-05-01 18:00:25 +0530

    Course offered ui

--- src/components/addCourse/index.jsx
+++ src/components/addCourse/index.jsx
@@ -1,17 +1,34 @@
-import React, { forwardRef, useContext, useEffect, useRef, useState } from "react";
+import React, {
+  forwardRef,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import config from "@/config";
 import axios from "axios";
 import ImageUploader from "../ImageUploder";
 import "./style.scss";
 import { UserContext } from "../../context/UserContext";
-import { TextField } from "@mui/material";
+import TextField from "@mui/material/TextField";
 import { CourseContext } from "../../context/CourseContext";
 import CustomDatePicker from "../customDatePicker";
+import { FaBookReader } from "react-icons/fa";
+import { MdAttachMoney } from "react-icons/md";
+import { GiDuration } from "react-icons/gi";
+
+import {
+  FormControl,
+  InputAdornment,
+  InputLabel,
+  MenuItem,
+  Select,
+} from "@mui/material";
 const { backend_url } = config;
 
 const AddCourse = forwardRef((props, ref) => {
-  const datepicker = useRef(null)
-  const { setAddCourseEnable } = useContext(CourseContext)
+  const datepicker = useRef(null);
+  const { setAddCourseEnable } = useContext(CourseContext);
 
   const [course, setCourse] = useState({
     name: "",
@@ -20,32 +37,31 @@ const AddCourse = forwardRef((props, ref) => {
     academic_session: { start_year: null, end_year: null },
     course_fee: "",
     course_duration_in_days: "",
-  })
+  });
   const { user } = useContext(UserContext);
   const [errors, setErrors] = useState({});
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setCourse(prev => ({ ...prev, [name]: value }));
+    setCourse((prev) => ({ ...prev, [name]: value }));
     // Clear errorrrr message when user starts typing in the field
     setErrors({ ...errors, [name]: "" });
   };
 
   const handleDateChange = (field, value) => {
-    setAddCourseEnable(prev => !prev)
-    setCourse(prev => ({
+    setAddCourseEnable((prev) => !prev);
+    setCourse((prev) => ({
       ...prev,
       academic_session: { ...prev.academic_session, [field]: value },
     }));
   };
 
   const handleAddCourse = async () => {
-
     const formData = new FormData();
     for (const key in course) {
       formData.append(key, course[key]);
     }
-
+    console.log(formData);
     try {
       const { data } = await axios.post(`${backend_url}/ep/courses`, formData, {
         headers: {
@@ -65,33 +81,40 @@ const AddCourse = forwardRef((props, ref) => {
 
   return (
     <div className="addcourse-container">
-      <div ref={ref} className="course_section">
+      <div className="course_section">
         <div className="main-container">
-          <div className="img_uploder">
+          {/* <div className="img_uploder">
             <ImageUploader />
-          </div>
+          </div> */}
           <div className="course-data">
             <div className="right-Section">
               <div className="course_details">
-                <label htmlFor="">Name:</label>
+                {/* <label htmlFor="">Name:</label> */}
               </div>
               <div className="course_input">
-                <input
-                  placeholder="enter Course Name"
+                <TextField fullWidth
+                  placeholder="    enter Course Name"
                   type="text"
                   name="name"
                   value={course.name}
                   onChange={handleChange}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <FaBookReader />
+                      </InputAdornment>
+                    ),
+                  }}
                 />
                 {errors.name && <div className="error">{errors.name}</div>}
               </div>
             </div>
             <div className="right-Section">
               <div className="course_details">
-                <label htmlFor="">Type:</label>
+                {/* <label htmlFor="">Type:</label> */}
               </div>
               <div className="course_input">
-                <select
+                {/* <select
                   name="type"
                   value={course.type}
                   onChange={handleChange}
@@ -99,15 +122,27 @@ const AddCourse = forwardRef((props, ref) => {
                   <option value="">Select Type</option>
                   <option value="UG">UG</option>
                   <option value="PG">PG</option>
-                </select>
+                </select> */}
+                <FormControl fullWidth>
+                  <InputLabel>Type</InputLabel>
+                  <Select
+                    name=" type"
+                    value={course.type}
+                    label="Age"
+                    onChange={handleChange}
+                  >
+                    <MenuItem value="UG">UG</MenuItem>
+                    <MenuItem value="PG">PG</MenuItem>
+                  </Select>
+                </FormControl>
                 {errors.type && <div className="error">{errors.type}</div>}
               </div>
             </div>
             <div className="right-Section">
               <div className="course_details">
-                <label htmlFor="">Academic Session:</label>
+                {/* <label htmlFor="">Academic Session:</label> */}
               </div>
-              <div className="course_input" style={{ width: "300px" }}>
+              <div className="course_input">
                 <CustomDatePicker
                   ref={datepicker}
                   label="Start Year"
@@ -131,15 +166,22 @@ const AddCourse = forwardRef((props, ref) => {
             </div>
             <div className="right-Section">
               <div className="course_details">
-                <label htmlFor="">Course Fee:</label>
+                {/* <label htmlFor="">Course Fee:</label> */}
               </div>
               <div className="course_input">
-                <input
-                  placeholder="Enter Fees"
+                <TextField fullWidth
+                  placeholder="    Enter Fees"
                   type="number"
                   name="course_fee"
                   value={course.course_fee}
                   onChange={handleChange}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <MdAttachMoney />
+                      </InputAdornment>
+                    ),
+                  }}
                 />
                 {errors.course_fee && (
                   <div className="error">{errors.course_fee}</div>
@@ -148,15 +190,22 @@ const AddCourse = forwardRef((props, ref) => {
             </div>
             <div className="right-Section">
               <div className="course_details">
-                <label htmlFor="">Course Duration:</label>
+                {/* <label htmlFor="">Course Duration:</label> */}
               </div>
               <div className="course_input">
-                <input
-                  placeholder="Enter Duration"
+                <TextField fullWidth
+                  placeholder="   Enter Duration"
                   type="number"
                   name="course_duration_in_days"
                   value={course.course_duration_in_days}
                   onChange={handleChange}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <GiDuration />
+                      </InputAdornment>
+                    ),
+                  }}
                 />
                 {errors.course_duration_in_days && (
                   <div className="error">{errors.course_duration_in_days}</div>
@@ -174,7 +223,7 @@ const AddCourse = forwardRef((props, ref) => {
               <button
                 className="cancel"
                 onClick={handleCancel}
-                style={{ backgroundColor: "#1F0A69" }}
+                // style={{ backgroundColor: "#1F0A69" }}
               >
                 Cancel
               </button>
