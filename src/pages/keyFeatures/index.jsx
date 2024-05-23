9a473735e875908aebe7a4278b0e3577fbcf8a83
Nikhil mandia <nikhilmandia7@gmail.com>
2024-05-23 15:08:14 +0530

    Sdsdf

--- src/pages/keyFeatures/index.jsx
+++ src/pages/keyFeatures/index.jsx
@@ -1,167 +1,205 @@
-import './style.scss';
-import KeyFeaturesChildren from '../../components/keyFeaturesChildren';
+import "./style.scss";
+import KeyFeaturesChildren from "../../components/keyFeaturesChildren";
 import axios from "axios";
-import config from "@/config"
+import config from "@/config";
 import { useState, useEffect, useContext } from "react";
-import { UserContext } from '../../context/UserContext';
-import { ProfileContext } from '../../context/ProfileContext';
+import { UserContext } from "../../context/UserContext";
+import { ProfileContext } from "../../context/ProfileContext";
 import { GoArrowSwitch } from "react-icons/go";
+import Spinner from "../../components/spinner/Index";
 
 const { backend_url } = config;
 
 const KeyFeatures = () => {
-    const [keyFeaturesInstitute, setKeyFeaturesInstitute] = useState([]);
-    const [remainingKeyFeatures, setRemainingKeyFeatures] = useState([]);
-    const { user } = useContext(UserContext);
-    const { editKeyFeatureEnable, setEditKeyFeatureEnable } = useContext(ProfileContext);
-    
+  const [keyFeaturesInstitute, setKeyFeaturesInstitute] = useState([]);
+  const [remainingKeyFeatures, setRemainingKeyFeatures] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const { user } = useContext(UserContext);
+  const { editKeyFeatureEnable, setEditKeyFeatureEnable } =
+    useContext(ProfileContext);
 
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const { data } = await axios.get(`${backend_url}/ep/key-features`, {
-                    headers: {
-                        Authorization: user.token
-                    }
-                });
-                setKeyFeaturesInstitute(data);
-                console.log(keyFeaturesInstitute)
-            } catch (error) {
-                console.log("Error Fetching Key Features");
-            }
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const { data } = await axios.get(`${backend_url}/ep/key-features`, {
+          headers: {
+            Authorization: user.token,
+          },
+        });
+        setKeyFeaturesInstitute(data);
+        console.log(keyFeaturesInstitute);
+      } catch (error) {
+        console.log("Error Fetching Key Features");
+      }
+    }
+    fetchData();
+  }, [user.token]);
+
+  useEffect(() => {
+    async function fetchRemainingFeatures() {
+      try {
+        const { data } = await axios.get(
+          `${backend_url}/admin/key-features-institute/remaining-key-features-for-institute`,
+          {
+            headers: {
+              Authorization: user.token,
+            },
+          }
+        );
+        setRemainingKeyFeatures(data);
+      } catch (error) {
+        console.log("Error fetching the remaining key features");
+      }
+    }
+    fetchRemainingFeatures();
+  }, [user.token]);
+
+  const handleDragStart = (e, item) => {
+    e.dataTransfer.setData("text/plain", JSON.stringify(item));
+  };
+
+  const handleDrop = (e, target) => {
+    e.preventDefault();
+    const item = JSON.parse(e.dataTransfer.getData("text/plain"));
+
+    // Check if the item is already in the target list
+    const isDuplicate =
+      target === "institute"
+        ? keyFeaturesInstitute.some((feature) => feature.name === item.name)
+        : remainingKeyFeatures.some((feature) => feature.name === item.name);
+
+    // If it's not a duplicate, update the lists accordingly
+    if (!isDuplicate) {
+      if (target === "institute") {
+        setKeyFeaturesInstitute((prevState) => [...prevState, item]);
+        setRemainingKeyFeatures((prevState) =>
+          prevState.filter((el) => el.name !== item.name)
+        );
+      } else {
+        setRemainingKeyFeatures((prevState) => [...prevState, item]);
+        setKeyFeaturesInstitute((prevState) =>
+          prevState.filter((el) => el.name !== item.name)
+        );
+      }
+    }
+  };
+  console.log(keyFeaturesInstitute);
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const handleSave = async () => {
+    try {
+      setLoading((prev) => !prev);
+      // Extract _id from each item in keyFeaturesInstitute and create new_key_features array
+      const new_key_features = keyFeaturesInstitute.map((feature) => ({
+        _id: feature._id,
+      }));
+
+      await axios.put(
+        `${backend_url}/ep/key-features/edit`,
+        { new_key_features },
+        {
+          headers: {
+            Authorization: user.token,
+          },
         }
-        fetchData();
-    }, [user.token]);
+      );
+      // Handle success
+      console.log("Key features updated successfully!");
+      setLoading((prev) => !prev);
 
-    useEffect(() => {
-        async function fetchRemainingFeatures() {
-            try {
-                const { data } = await axios.get(`${backend_url}/admin/key-features-institute/remaining-key-features-for-institute`, {
-                    headers: {
-                        Authorization: user.token
-                    }
-                });
-                setRemainingKeyFeatures(data);
-            } catch (error) {
-                console.log("Error fetching the remaining key features");
-            }
-        }
-        fetchRemainingFeatures();
-    }, [user.token]);
+      setEditKeyFeatureEnable(false); // Disable editing mode after successful save
+    } catch (error) {
+      setLoading((prev) => !prev);
 
-    const handleDragStart = (e, item) => {
-        e.dataTransfer.setData('text/plain', JSON.stringify(item));
-    };
+      console.log("Error updating key features:", error);
+      // Handle error
+    }
+  };
 
-    const handleDrop = (e, target) => {
-        e.preventDefault();
-        const item = JSON.parse(e.dataTransfer.getData('text/plain'));
-        
-        // Check if the item is already in the target list
-        const isDuplicate = target === 'institute' ? 
-            keyFeaturesInstitute.some(feature => feature.name === item.name) :
-            remainingKeyFeatures.some(feature => feature.name === item.name);
-    
-        // If it's not a duplicate, update the lists accordingly
-        if (!isDuplicate) {
-            if (target === 'institute') {
-                setKeyFeaturesInstitute(prevState => [...prevState, item]);
-                setRemainingKeyFeatures(prevState => prevState.filter(el => el.name !== item.name));
-            } else {
-                setRemainingKeyFeatures(prevState => [...prevState, item]);
-                setKeyFeaturesInstitute(prevState => prevState.filter(el => el.name !== item.name));
-            }
-        }
-    };
-    console.log(keyFeaturesInstitute)
-    const handleDragOver = (e) => {
-        e.preventDefault();
-    };
-
-    const handleSave = async () => {
-        try {
-            // Extract _id from each item in keyFeaturesInstitute and create new_key_features array
-            const new_key_features = keyFeaturesInstitute.map(feature => ({ _id: feature._id }));
-            
-            await axios.put(`${backend_url}/ep/key-features/edit`, { new_key_features }, {
-                headers: {
-                    Authorization: user.token
-                }
-            });
-            // Handle success
-            console.log("Key features updated successfully!");
-            setEditKeyFeatureEnable(false); // Disable editing mode after successful save
-        } catch (error) {
-            console.log("Error updating key features:", error);
-            // Handle error
-        }
-    };
-    
-
-    return (
-        <div className="key-features-parent">
-            <div className="key-features-head">
-                <h1>Key Features</h1>
-                {!editKeyFeatureEnable ?
-                    <button className='kf-edit' onClick={() => setEditKeyFeatureEnable(true)}>Edit</button>
-                    :
-                    <div className='cancel-save-btns'>
-                        <button className='kf-save' onClick={handleSave}>Save</button>
-                        <button className='kf-cancel' onClick={() => setEditKeyFeatureEnable(false)}>Cancel</button>
-                    </div>
-                }
+  return (
+    <div className="key-features-parent">
+      <div className="key-features-head">
+        <h1>Key Features</h1>
+        {!editKeyFeatureEnable ? (
+          <button
+            className="kf-edit"
+            onClick={() => setEditKeyFeatureEnable(true)}
+          >
+            Edit
+          </button>
+        ) : (
+          <div className="cancel-save-btns">
+            <button className="kf-save" onClick={handleSave}>
+              {loading ? <Spinner /> : "Save"}
+            </button>
+            <button
+              className="kf-cancel"
+              onClick={() => setEditKeyFeatureEnable(false)}
+            >
+              Cancel
+            </button>
+          </div>
+        )}
+      </div>
+      {editKeyFeatureEnable ? (
+        <div className="edit-key-features">
+          <div
+            className="key-features-child key-features-remaining"
+            onDrop={(e) => handleDrop(e, "remaining")}
+            onDragOver={handleDragOver}
+          >
+            <div className="key-features-child-heading">
+              <h5>All Available Key Features</h5>
             </div>
-            {editKeyFeatureEnable ?
-                <div className="edit-key-features">
-                    
-                    <div className="key-features-child key-features-remaining"
-                         onDrop={(e) => handleDrop(e, 'remaining')}
-                         onDragOver={handleDragOver}>
-                        <div className="key-features-child-heading">
-                            <h5>All Available Key Features</h5>
-                        </div>
-                        {remainingKeyFeatures.map((remainingKeyFeature, i) => (
-                            <KeyFeaturesChildren
-                                key={i}
-                                featureName={remainingKeyFeature.name}
-                                featurePng={remainingKeyFeature.key_features_icon}
-                                draggable
-                                onDragStart={(e) => handleDragStart(e, remainingKeyFeature)}
-                            />
-                        ))}
-                    </div>
-                    <GoArrowSwitch />
-                    <div className="key-features-child key-features-existing"
-                         onDrop={(e) => handleDrop(e, 'institute')}
-                         onDragOver={handleDragOver}>
-                        <div className="key-features-child-heading">
-                            <h5>Your Key Features</h5>
-                        </div>
-                        {keyFeaturesInstitute.map((keyFeatureInstitute, i) => (
-                            <KeyFeaturesChildren
-                                key={i}
-                                featureName={keyFeatureInstitute.name}
-                                featurePng={keyFeatureInstitute.key_features_icon}
-                                draggable
-                                onDragStart={(e) => handleDragStart(e, keyFeatureInstitute)}
-                            />
-                        ))}
-                    </div>
-                </div>
-                :
-                
-                <div className="key-features-child">
-                    {keyFeaturesInstitute.length === 0 ? (
-                        <p>You don't have any key features. Please click on edit to add.</p>
-                    ) : (
-                        keyFeaturesInstitute.map((keyFeatureInstitute, i) => (
-                            <KeyFeaturesChildren key={i} featureName={keyFeatureInstitute.name} featurePng={keyFeatureInstitute.key_features_icon} />
-                        ))
-                    )}
-                </div>
-            }
+            {remainingKeyFeatures.map((remainingKeyFeature, i) => (
+              <KeyFeaturesChildren
+                key={i}
+                featureName={remainingKeyFeature.name}
+                featurePng={remainingKeyFeature.key_features_icon}
+                draggable
+                onDragStart={(e) => handleDragStart(e, remainingKeyFeature)}
+              />
+            ))}
+          </div>
+          <GoArrowSwitch />
+          <div
+            className="key-features-child key-features-existing"
+            onDrop={(e) => handleDrop(e, "institute")}
+            onDragOver={handleDragOver}
+          >
+            <div className="key-features-child-heading">
+              <h5>Your Key Features</h5>
+            </div>
+            {keyFeaturesInstitute.map((keyFeatureInstitute, i) => (
+              <KeyFeaturesChildren
+                key={i}
+                featureName={keyFeatureInstitute.name}
+                featurePng={keyFeatureInstitute.key_features_icon}
+                draggable
+                onDragStart={(e) => handleDragStart(e, keyFeatureInstitute)}
+              />
+            ))}
+          </div>
         </div>
-    );
+      ) : (
+        <div className="key-features-child">
+          {keyFeaturesInstitute.length === 0 ? (
+            <p>You don't have any key features. Please click on edit to add.</p>
+          ) : (
+            keyFeaturesInstitute.map((keyFeatureInstitute, i) => (
+              <KeyFeaturesChildren
+                key={i}
+                featureName={keyFeatureInstitute.name}
+                featurePng={keyFeatureInstitute.key_features_icon}
+              />
+            ))
+          )}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default KeyFeatures;
